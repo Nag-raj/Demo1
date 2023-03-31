ae9d5e260614f5be2979a4efad9ecb7977da9292
Nag-raj <115706032+Nag-raj@users.noreply.github.com>
2023-03-31 15:52:01 +0530

    deleted aem code

--- core/src/main/java/com/olympus/oca/core/models/CartDetailsModel.java
+++
@@ -1,6 +0,0 @@
-package com.olympus.oca.core.models;
-
-public interface CartDetailsModel {
-	public String getCheckoutPagePath();
-	public String getShopPagePath();
-}
--- core/src/main/java/com/olympus/oca/core/models/OlympusProductList.java
+++
@@ -1,7 +0,0 @@
-package com.olympus.oca.core.models;
-
-import com.adobe.cq.commerce.core.components.models.productlist.ProductList;
-
-public interface OlympusProductList extends ProductList {
-
-}
